180fe1c991c5cc97b2115e1f52ac838a574f13a7
ArkanUbaidillah <arkanwarman@gmail.com>
2026-05-13 20:39:48 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/sidiq/", // Gunakan path absolut untuk subfolder agar routing lancar
+  base: "/", // Subdomain deployment: https://sidiq.ifportofolio.com/
 });
